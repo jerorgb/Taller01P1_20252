bd64a279997999807dbe183200cb4c2baa4c6a73
jerorgb <146040805+jerorgb@users.noreply.github.com>
2025-08-02 11:25:36 -0500

    main

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    director = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+
+def __str__(self):
+    return self.title
+
 # Create your models here.
--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Movie
 
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+        return render(request, 'home.html', {'movies': movies, 'searchTerm': searchTerm})
+    
 
 def about(request):
     return render (request, 'about.html')
