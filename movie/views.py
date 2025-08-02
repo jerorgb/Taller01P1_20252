4139dc30da9ceec069ef2943a78b2f22c1695940
jerorgb <146040805+jerorgb@users.noreply.github.com>
2025-08-02 12:56:33 -0500

    main

--- movie/views.py
+++ movie/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
-    return render(request, 'home.html')
+    #return render(request, 'home.html')
+    SearchTerm = request.GET.get('searchMovie')
+    if SearchTerm:
+        movies = Movie.objects.filter(title__icontains=SearchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies})
+
 
 def about(request):
     return render (request, 'about.html')
